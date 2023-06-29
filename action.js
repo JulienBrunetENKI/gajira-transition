73296de0a799e303fa94601fd0f0383907a8c89a
julien brunet <julien.brunet@ext.leroymerlin.fr>
2023-06-29 16:57:16 +0200

    add-fields-support

--- action.js
+++ action.js
@@ -38,9 +38,17 @@ module.exports = class {
     console.log(`Selected transition:${JSON.stringify(transitionToApply, null, 4)}`)
 
     await this.Jira.transitionIssue(issueId, {
-      transition: {
-        id: transitionToApply.id,
-      },
+			transition: {
+				id: transitionToApply.id,
+			},
+			fields: {
+				resolution: {
+					self: "https://partners-iot-leroymerlin.atlassian.net/rest/api/2/resolution/10107",
+					id: "10107",
+					description: "Work has been completed on this issue.",
+					name: "Done"
+				}
+			},
     })
 
     const transitionedIssue = await this.Jira.getIssue(issueId)
